390dfa956b09fe4cee6ade84d89d6bffd0d648da
Prince Tagadiya <princetagadiya11@gmail.com>
2025-08-21 23:39:48 +0530

    update middleware

--- middleware.ts
+++ middleware.ts
@@ -6,10 +6,12 @@ export default authMiddleware({
     '/events/:id'
   ],
   ignoredRoutes: [
-    '/api/webhook(.*)'
+    '/api/webhooks/clerk',
+    '/api/webhooks/stripe',
+    '/api/uploadthing'
   ]
 });
  
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/"],
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 };
