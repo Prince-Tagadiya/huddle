6569ca940e46735eea07b9e08a48197eaaa2c9fe
Prince Tagadiya <princetagadiya11@gmail.com>
2025-08-22 00:27:28 +0530

    event mongo update

--- lib/database/models/event.model.ts
+++ lib/database/models/event.model.ts
@@ -1,4 +1,4 @@
-import { Document, Schema, model, models } from "mongoose";
+import { Document, Schema, model, models, Types } from "mongoose";
 
 export interface IEvent extends Document {
   _id: string;
@@ -9,28 +9,28 @@ export interface IEvent extends Document {
   imageUrl: string;
   startDateTime: Date;
   endDateTime: Date;
-  price: string;
+  price?: string;
   isFree: boolean;
   url?: string;
-  category: { _id: string, name: string }
-  organizer: { _id: string, firstName: string, lastName: string }
+  category: Types.ObjectId | { _id: string; name: string };
+  organizer: Types.ObjectId | { _id: string; firstName: string; lastName: string };
 }
 
-const EventSchema = new Schema({
+const EventSchema = new Schema<IEvent>({
   title: { type: String, required: true },
   description: { type: String },
   location: { type: String },
   createdAt: { type: Date, default: Date.now },
   imageUrl: { type: String, required: true },
-  startDateTime: { type: Date, default: Date.now },
-  endDateTime: { type: Date, default: Date.now },
+  startDateTime: { type: Date, required: true },
+  endDateTime: { type: Date, required: true },
   price: { type: String },
   isFree: { type: Boolean, default: false },
   url: { type: String },
-  category: { type: Schema.Types.ObjectId, ref: 'Category' },
-  organizer: { type: Schema.Types.ObjectId, ref: 'User' },
-})
+  category: { type: Schema.Types.ObjectId, ref: "Category", required: true },
+  organizer: { type: Schema.Types.ObjectId, ref: "User", required: true },
+});
 
-const Event = models.Event || model('Event', EventSchema);
+const Event = models.Event || model<IEvent>("Event", EventSchema);
 
 export default Event;
